297b23aa047ed1687a85749086460af754a75de4
anuragdhingra <anuragdhingra101@gmail.com>
2019-02-12 14:48:20 +0900

    Added tests for Getusers

--- pkg/user/handler_test.go
+++ pkg/user/handler_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/anuragdhingra/go-rest-example/db/models"
 	"github.com/go-chi/chi"
+	"io"
 	"net/http/httptest"
 	"testing"
 )
@@ -13,11 +14,9 @@ func TestSignup(t *testing.T) {
 	router := chi.NewRouter()
 	router.Post("/signup", Signup)
 
-	user := &models.User{Username:"testuser", Email:"testemail@gmail.com", Password:"password"}
-	userJSON, _ := json.Marshal(user)
-	jsonReader := bytes.NewReader(userJSON)
+	user := createTestUser()
 
-	request := httptest.NewRequest("POST","/signup", jsonReader)
+	request := httptest.NewRequest("POST","/signup", user)
 	writer := httptest.NewRecorder()
 
 	router.ServeHTTP(writer, request)
@@ -25,4 +24,34 @@ func TestSignup(t *testing.T) {
 	if writer.Code != 200 {
 		t.Errorf("Response code is %v", writer.Code)
 	}
+}
+
+func TestGetUsers(t *testing.T) {
+
+	router := chi.NewRouter()
+	router.Get("/users", GetUsers)
+
+	request := httptest.NewRequest("GET","/users", nil)
+	writer := httptest.NewRecorder()
+
+	router.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	var users []models.User
+	json.Unmarshal(writer.Body.Bytes(), &users)
+	for _,user := range users {
+		if user.Username != "testuser" {
+			t.Error("Cannot retrieve JSON post")
+		}
+	}
+}
+
+func createTestUser() (jsonReader io.Reader) {
+	user := &models.User{Username:"testuser", Email:"testemail@gmail.com", Password:"password"}
+	userJSON, _ := json.Marshal(user)
+	jsonReader = bytes.NewReader(userJSON)
+	return jsonReader
 }
